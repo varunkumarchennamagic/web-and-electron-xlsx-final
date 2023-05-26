a827f0acbc2521e6bf4d2153176a8997ef260784
Varun Kumar Chenna <varun.kumar@magicedtech.com>
2023-05-26 21:37:06 +0530

    error being saved to seperate log file

--- app.js
+++ app.js
@@ -18,26 +18,68 @@ function convertToJSON() {
 
     // Convert the first sheet to JSON
     const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-    const jsonData = XLSX.utils.sheet_to_json(worksheet);
-    var newJson = jsonData[0];
-    var currKey = "";
-    jsonData.forEach((row) => {
-      if (!row.definition) {
-        currKey = row["key term"];
-      } else {
-        if (!newJson[currKey]) {
-          newJson[currKey] = [];
+    const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+    // Define the list of allowed exceptions
+    const allowedExceptions = ['-', '_', ' ', '.'];
+
+    // Check for special characters and cell length
+    const invalidChars = new RegExp(`[^\\w${allowedExceptions.join('\\\\')}]`, 'g');
+    let invalidCells = [];
+
+    jsonData.forEach((row, rowIndex) => {
+      row.forEach((cell, columnIndex) => {
+        if (typeof cell === 'string') {
+          if (invalidChars.test(cell) || cell.length > 128) {
+            const invalidChar = invalidChars.test(cell) ? cell.match(invalidChars)[0] : null;
+            const columnHeader = jsonData[0][columnIndex]; // Get the header from the first row
+            invalidCells.push({
+              row: rowIndex + 1,
+              column: columnHeader,
+              character: invalidChar,
+              lengthExceeded: cell.length > 128,
+            });
+          }
         }
-        newJson[currKey].push(row);
-      }
+      });
     });
+
+    if (invalidCells.length > 0) {
+      let errorMessage = 'Error: Special characters or cell length exceeded in the following cells:\n';
+
+      invalidCells.forEach((cell) => {
+        errorMessage += `Row: ${cell.row}, Column: ${cell.column}`;
+
+        if (cell.character) {
+          errorMessage += `, Character: "${cell.character}"`;
+        }
+
+        if (cell.lengthExceeded) {
+          errorMessage += `, Length Exceeded`;
+        }
+
+        errorMessage += '\n';
+      });
+
+      // Show user-friendly error message
+      alert('Invalid data found in the uploaded file. Please check the error log for more details.');
+
+      // Write detailed error to error log file
+      const errorLog = `logs/error_${new Date().toISOString()}.txt`;
+      const errorContent = `Error Log - ${new Date().toISOString()}\n\n${errorMessage}`;
+
+      const blob = new Blob([errorContent], { type: 'text/plain;charset=utf-8' });
+      saveAs(blob, errorLog);
+
+      return;
+    }
 
     // Enable the download button
     downloadBtn.disabled = false;
 
     // Handle the download button click
     downloadBtn.addEventListener('click', function () {
-      const jsonContent = JSON.stringify(newJson, null, 2);
+      const jsonContent = JSON.stringify(jsonData, null, 2);
       const blob = new Blob([jsonContent], { type: 'application/json' });
       saveAs(blob, 'data.json');
     });
